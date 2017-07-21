49fd8d1d2c9ca86a2fc37750b88e4ccc190ded50
christikaes <christikaes@gmail.com>
2017-07-21 04:09:10 -0400

    AngularFire

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,37 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { }
 
   // Returns an observable stream of whether or not the user is logged in
-  public $auth(): Observable<boolean>{
-    // TODO!!
-    return new Observable((observer) => {
-      observer.next(false);
-    })
+  public $auth(): Observable<boolean> {
+    return this.afAuth.authState.map((auth) => {
+      return !!auth
+    });
   }
 
   // Returns an observable strem of success/errors when logging in
   public $login(email: string, password: string): Observable<any> {
-    // TODO!!
     return new Observable((observer) => {
-      observer.next({error: "Not implemented"});
+      this.afAuth.auth.signInWithEmailAndPassword(email, password)
+        .catch(error => {
+          observer.next({ error: error.message });
+        })
+        .then(success => {
+          observer.next({ success: success })
+        })
     })
   }
 
   // Returns an observable strem of success/errors when registering in
   public $register(email: string, password: string): Observable<any> {
-    // TODO!!
     return new Observable((observer) => {
-      observer.next({error: "Not implemented"});
+      this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+        .catch(error => {
+          observer.next({ error: error.message });
+        })
+        .then(success => {
+          observer.next({ success: success })
+        })
     })
   }
 
   // Returns an observable strem of success/errors when logging out
   public logout() {
-    // TODO!!
+    this.afAuth.auth.signOut();
   }
 }
--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,34 +1,76 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class DatabaseService {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afDatabase: AngularFireDatabase
+  ) { }
 
   // Returns an observable stream of an array of the current user's musical boxes
   public $myMusicalBoxList() {
-    // TODO!!
     return new Observable((observer) => {
-      observer.next([])
+      this.afAuth.authState.subscribe((user) => {
+        if (user) {
+          const uid = user.uid;
+          this.afDatabase.list('players/' + uid).subscribe(boxList => {
+            observer.next(boxList);
+          })
+        } else {
+          observer.next([])
+        }
+      })
+      observer.next([]);
     })
   }
 
-  // Returns an observable stream of an array of all the public musical boxes
+  // Returns an observablestream of an array of all the public musical boxes
   public $publicMusicalBoxList() {
-    // TODO!!
     return new Observable((observer) => {
-      observer.next([])
+      this.afDatabase.list('players/').subscribe(players => {
+        const boxList = [];
+        players.forEach(player => {
+          Object.keys(player).map(key => player[key]).forEach( box => {
+            if (box.public) {
+              boxList.push(box);
+            }
+          })
+        })
+        observer.next(boxList);
+      })
+      observer.next([]);
     })
   }
 
   // Returns an observable stream of the updated Box
-  public $updateBox(box){
-    // TODO!!
+  public $updateBox(box) {
     return new Observable((observer) => {
-      observer.next({ box });
+      const user = this.afAuth.auth.currentUser;
+      if (user) {
+        // If the user is logged in
+        const uid = user.uid;
+        if (box.id) {
+          // This is a previously saved box, so update it
+          this.afDatabase.object('players/' + uid + '/' + box.id)
+            .set(box)
+            .then(() => observer.next({ box }))
+            .catch(error => observer.next({ error: error.message }));
+        } else {
+          // This box has not been saved before, so add it
+          box.id = this.afDatabase.list('players/' + uid).push(box).key
+          observer.next({ box });
+        }
+      } else {
+        observer.next({
+          error: 'User not logged in',
+          box
+        });
+      }
     })
   }
-
 }
 
